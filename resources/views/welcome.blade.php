50ab808f412092797c6abfd3a1b4675495644776
victor <victor.fernandez@geobyte.cl>
2021-08-01 02:13:15 -0400

    update comentario inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,6 @@
 
 @section('content')
 <div class="container-fluid">
-    <p>Hola que tal, mi nombre es Víctor Fernández y me gusta programar.</p>
+    <p>Hola que tal, mi nombre es Víctor Fernández, si bien ciertos compromisos laborales me ocuparon bastante tiempo de las 48hrs, los desafíos fueron muy divertidos y retadores, me sirvieron bastante para mejorar.</p>
 </div>
 @endsection
